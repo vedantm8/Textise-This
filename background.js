ce76bffc1805d45b746b989bf0c7eff8cc7383a8
Vedant Mukherjee <vedantm8@gmail.com>
2022-02-24 19:37:36 -0500

    adding redirect utility

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+// find the current URL 
+let currURL;
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => { currURL = tabs[0].url; });
+
+// Create a newe tab, and set the URL of the new tab using textise 
 document.getElementById("textise-button").addEventListener("click", function () {
-    alert("Button clicked!")
+    currURL = currURL.replace(/^https?:\/\//, '')
+    chrome.tabs.create({ url: `https://www.textise.net/showText.aspx?strURL=${currURL}` })
 });
